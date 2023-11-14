0a75f1e0100b3303654d496c4018af29c72eb622
BoneInscri <20051124@hdu.edu.cn>
2023-11-14 17:46:43 +0800

    fix some bug

--- ics2022/nemu/src/cpu/cpu-exec.c
+++ ics2022/nemu/src/cpu/cpu-exec.c
@@ -41,12 +41,12 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
-#ifdef CONFIG_CC_WATCHPOINT
+// #ifdef CONFIG_CC_WATCHPOINT
   int ret = trace_watchpoint();
   if(ret) {
     nemu_state.state = NEMU_STOP;  
   }
-#endif
+// #endif
 
 }
 
