d26cc95eee86dcbe84aa22441978dbc4f64a629f
BoneInscri <20051124@hdu.edu.cn>
2023-11-14 18:38:41 +0800

    add am-kernels

--- ics2022/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/logo.d
+++
@@ -1,9 +0,0 @@
-cmd_/media/boneinscri/Expand_1/HDU/南大PA/ics2022_git/ics-pa/ics2022/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/logo.o := unused
-
-source_/media/boneinscri/Expand_1/HDU/南大PA/ics2022_git/ics-pa/ics2022/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/logo.o := src/isa/riscv64/logo.c
-
-deps_/media/boneinscri/Expand_1/HDU/南大PA/ics2022_git/ics-pa/ics2022/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/logo.o := \
-
-/media/boneinscri/Expand_1/HDU/南大PA/ics2022_git/ics-pa/ics2022/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/logo.o: $(deps_/media/boneinscri/Expand_1/HDU/南大PA/ics2022_git/ics-pa/ics2022/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/logo.o)
-
-$(deps_/media/boneinscri/Expand_1/HDU/南大PA/ics2022_git/ics-pa/ics2022/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/logo.o):
