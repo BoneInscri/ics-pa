92ee845ea138954d236a4de729f82a0477925cdd
tracer-ics2022 <tracer@njuics.org>
2023-11-15 18:44:40 +0800

    >  compile NEMU 20051124 BoneInscri Linux boneinscri-ubuntu 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:44:40 up 42 min,  1 user,  load average: 0.67, 0.62, 0.63

--- ics2022/nemu/src/cpu/cpu-exec.c
+++ ics2022/nemu/src/cpu/cpu-exec.c
@@ -74,7 +74,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
 // #ifdef CONFIG_ITRACE_COND
 //   // if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 // #endif
-  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(logbuf[itrace_p])); }
+  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(logbuf[itrace_p-1])); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
   // #ifdef CONFIG_CC_WATCHPOINT
