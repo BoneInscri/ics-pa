e65619b37ba88a0e8a0d81d8cbf2e26c20949302
tracer-ics2022 <tracer@njuics.org>
2023-11-15 18:42:17 +0800

    >  compile NEMU 20051124 BoneInscri Linux boneinscri-ubuntu 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:42:17 up 39 min,  1 user,  load average: 0.39, 0.62, 0.63

--- ics2022/nemu/src/monitor/sdb/sdb.c
+++ ics2022/nemu/src/monitor/sdb/sdb.c
@@ -252,7 +252,14 @@ extern int itrace_p;
 static int cmd_itrace(char *args)
 {
   for(int i = 0;i<ITRACE_SIZE;i++) {
-    log_write("%s\n", logbuf[i]);
+    if(logbuf[i]) {
+      if(i!=itrace_p) {
+        log_write("      ");
+      } else  {
+        log_write("  --> ");
+      }
+      log_write("%s\n", logbuf[i]);
+    }
   } 
   return 0;
 }
