d66e6a97385d2e852718c01c0616c09e93fc68b8
tracer-ics2022 <tracer@njuics.org>
2023-11-15 11:53:43 +0800

    >  compile NEMU 20051124 BoneInscri Linux boneinscri-ubuntu 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:53:43 up  2:19,  1 user,  load average: 1.44, 0.80, 0.58

--- ics2022/abstract-machine/klib/src/string.c
+++ ics2022/abstract-machine/klib/src/string.c
@@ -4,43 +4,77 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
+size_t strlen(const char *s)
+{
+  // panic("Not implemented");
+  size_t len = 0;
+  while (s[len])
+  {
+    len++;
+  }
+  return len;
+}
+
+char *strcpy(char *dst, const char *src)
+{
   panic("Not implemented");
 }
 
-char *strcpy(char *dst, const char *src) {
+char *strncpy(char *dst, const char *src, size_t n)
+{
   panic("Not implemented");
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
+char *strcat(char *dst, const char *src)
+{
   panic("Not implemented");
 }
 
-char *strcat(char *dst, const char *src) {
+int strcmp(const char *s1, const char *s2)
+{
+  size_t s1_len = strlen(s1);
+  size_t s2_len = strlen(s2);
+  if (s1_len < s2_len)
+    return -1;
+  else if (s1_len > s2_len)
+    return 1;
+  while (s1 && s2)
+  {
+    if (*s1 < *s2)
+    {
+      return -1;
+    }
+    else if (*s1 > *s2)
+    {
+      return 1;
+    }
+  }
+  return 0;
+  // panic("Not implemented");
+}
+
+int strncmp(const char *s1, const char *s2, size_t n)
+{
   panic("Not implemented");
 }
 
-int strcmp(const char *s1, const char *s2) {
+void *memset(void *s, int c, size_t n)
+{
   panic("Not implemented");
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
+void *memmove(void *dst, const void *src, size_t n)
+{
   panic("Not implemented");
 }
 
-void *memset(void *s, int c, size_t n) {
+void *memcpy(void *out, const void *in, size_t n)
+{
   panic("Not implemented");
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
-}
-
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
-}
-
-int memcmp(const void *s1, const void *s2, size_t n) {
+int memcmp(const void *s1, const void *s2, size_t n)
+{
   panic("Not implemented");
 }
 
