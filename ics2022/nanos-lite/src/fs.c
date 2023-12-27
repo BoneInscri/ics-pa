b3929136cf0a94e825eaa88bcb84cbf9b981b3f9
BoneInscri <20051124@hdu.edu.cn>
2023-12-27 16:55:48 +0800

    vfs support serial

--- ics2022/nanos-lite/src/fs.c
+++ ics2022/nanos-lite/src/fs.c
@@ -1,4 +1,5 @@
 #include <fs.h>
+#include <device.h>
 
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
@@ -39,8 +40,8 @@ size_t invalid_write(const void *buf, size_t offset, size_t len)
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
     [FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
-    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-    [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
+    [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
@@ -67,11 +68,6 @@ int fs_open(const char *pathname, int flags, int mode)
 
 size_t fs_read(int fd, void *buf, size_t len)
 {
-  if (fd == FD_STDIN || fd == FD_STDOUT || fd == FD_STDERR)
-  {
-    panic("fs_read : not tested");
-  }
-
   size_t offset_disk = file_table[fd].disk_offset;
   size_t open_offset = file_table[fd].open_offset;
   size_t size = file_table[fd].size;
@@ -79,7 +75,7 @@ size_t fs_read(int fd, void *buf, size_t len)
   {
     panic("fd_read : over size");
   }
-  ramdisk_read(buf, offset_disk + open_offset, len);
+  file_table[fd].read(buf, offset_disk + open_offset, len);
   file_table[fd].open_offset += len;
 
   return len;
@@ -87,30 +83,17 @@ size_t fs_read(int fd, void *buf, size_t len)
 
 size_t fs_write(int fd, const void *buf, size_t len)
 {
-  if (fd == FD_STDIN)
+  size_t offset_disk = file_table[fd].disk_offset;
+  size_t open_offset = file_table[fd].open_offset;
+  size_t size = file_table[fd].size;
+  if (open_offset > size)
   {
-    panic("fs_write : not tested");
-  }
-  if (fd == FD_STDOUT || fd == FD_STDERR)
-  {
-    for (int i = 0; i < len; i++)
-    {
-      putch(((char *)buf)[i]);
-    }
-  }
-  else
-  {
-    size_t offset_disk = file_table[fd].disk_offset;
-    size_t open_offset = file_table[fd].open_offset;
-    size_t size = file_table[fd].size;
-    if (open_offset > size)
-    {
-      panic("fd_write : over size");
-    }
-    ramdisk_write(buf, offset_disk + open_offset, len);
-    file_table[fd].open_offset += len;
+    panic("fd_write : over size");
   }
 
+  file_table[fd].write(buf, offset_disk + open_offset, len);
+  file_table[fd].open_offset += len;
+ 
   return len;
 }
 
